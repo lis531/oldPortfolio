97ac5653a4ea4f80d9a2ca579bbd8064ef3c422d
lis531 <lisekminecraftowy175@gmail.com>
2023-03-05 15:55:59 +0100

    update #4
    lepsze popup'y, lepsza responsywność

--- src/projects.jsx
+++ src/projects.jsx
@@ -1,57 +1,66 @@
 import React, { useState } from 'react';
 
+const projectsInfo = {
+  SnakeTHEgame: {
+    header: 'Snake the game',
+    paragraph: 'A simple snake game made with C# in Unity. The game will be released this year.'
+  },
+  Calendar: {
+    header: 'Calendar',
+    paragraph: 'A calendar made with HTML, CSS and JavaScript with a lot of statistics. TBA.'
+  },
+  GoodToKnow: {
+    header: 'GoodToKnow',
+    paragraph: 'An app made with HTML, CSS and JavaScript using OpenAI. TBA.'
+  },
+  PhoneWidgets: {
+    header: 'Phone Widgets',
+    paragraph: 'An app made with ???. TBA.'
+  }
+};
+
 const Projects = () => {
-  const [modal, setModal] = useState(false);
-  const [modalHeader, setModalHeader] = useState('');
-  const [modalPar, setModalPar] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalContent, setModalContent] = useState({
+    header: '',
+    paragraph: ''
+  });
 
   const toggleModal = (id) => {
-    setModal(!modal);
-    
-    if(id === 'SnakeTHEgame') {
-      setModalHeader('Snake the game');
-      setModalPar('A simple snake game made with HTML, CSS and JavaScript. The game is fully responsive and works on all devices.');
-    } else if(id === 'Calendar') {
-      setModalHeader('Calendar');
-      setModalPar('A simple calendar made with HTML, CSS and JavaScript. The calendar is fully responsive and works on all devices.');
-    } else if(id === 'GoodToKnow') {
-      setModalHeader('GoodToKnow');
-      setModalPar('A simple app made with HTML, CSS and JavaScript. The app is fully responsive and works on all devices.');
-    } else if(id === 'PhoneWidgets') {
-      setModalHeader('Phone Widgets');
-      setModalPar('A simple app made with HTML, CSS and JavaScript. The app is fully responsive and works on all devices.');
+    setModalVisible(!modalVisible);
+
+    if (id) {
+      const { header, paragraph } = projectsInfo[id];
+      setModalContent({ header, paragraph });
     }
   };
 
-  if(modal) {
-    document.body.classList.add('active-modal');
-  } else {
-    document.body.classList.remove('active-modal');
-  }
-  
+  const modalClassName = `modal ${modalVisible ? '' : 'hidden'}`;
+
   return (
     <>
       <div className="Projects" id="projects">
         <h1>My Projects</h1>
         <div className="wrapper">
-          <button className="box" id="SnakeTHEgame" onClick={() => toggleModal("SnakeTHEgame")}><span>Snake the game</span></button>
-          <button className="box" id="Calendar" onClick={() => toggleModal("Calendar")}><span>Calendar</span></button>
+          <button className="box" id="SnakeTHEgame" onClick={() => toggleModal('SnakeTHEgame')}><span>Snake the game</span></button>
+          <button className="box" id="Calendar" onClick={() => toggleModal('Calendar')}><span>Calendar</span></button>
         </div>
         <div className="wrapper">
-          <button className="box" id="GoodToKnow" onClick={() => toggleModal("GoodToKnow")}><span>GoodToKnow</span></button>
-          <button className="box" id="PhoneWidgets" onClick={() => toggleModal("PhoneWidgets")}><span>Phone Widgets</span></button>
+          <button className="box" id="GoodToKnow" onClick={() => toggleModal('GoodToKnow')}><span>GoodToKnow</span></button>
+          <button className="box" id="PhoneWidgets" onClick={() => toggleModal('PhoneWidgets')}><span>Phone Widgets</span></button>
         </div>
       </div>
-      {modal && (
-        <div className="modal">
+      <div className={modalClassName}>
+        {modalVisible && (
           <div className="modal-content">
-            <h2 className='modal-header'>{modalHeader}</h2>
-            <p className='modal-par'>{modalPar}</p>
-            <button className="close-snake" onClick={() => toggleModal()}>CLOSE</button>
+            <h2 className='modal-header'>{modalContent.header}</h2>
+            <p className='modal-par'>{modalContent.paragraph}</p>
+            <button className="close-btn" onClick={() => toggleModal()}>CLOSE</button>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </>
   )
-}
-export default Projects
+};
+
+export default Projects;
