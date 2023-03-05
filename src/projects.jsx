b07380188b0b7d88ddb0ec6e5fcbe4cccdf2d770
lis531 <lisekminecraftowy175@gmail.com>
2023-03-05 14:23:47 +0100

    update #3
    popup'y, poprawki

--- src/projects.jsx
+++ src/projects.jsx
@@ -1,18 +1,57 @@
+import React, { useState } from 'react';
+
 const Projects = () => {
+  const [modal, setModal] = useState(false);
+  const [modalHeader, setModalHeader] = useState('');
+  const [modalPar, setModalPar] = useState('');
+
+  const toggleModal = (id) => {
+    setModal(!modal);
+    
+    if(id === 'SnakeTHEgame') {
+      setModalHeader('Snake the game');
+      setModalPar('A simple snake game made with HTML, CSS and JavaScript. The game is fully responsive and works on all devices.');
+    } else if(id === 'Calendar') {
+      setModalHeader('Calendar');
+      setModalPar('A simple calendar made with HTML, CSS and JavaScript. The calendar is fully responsive and works on all devices.');
+    } else if(id === 'GoodToKnow') {
+      setModalHeader('GoodToKnow');
+      setModalPar('A simple app made with HTML, CSS and JavaScript. The app is fully responsive and works on all devices.');
+    } else if(id === 'PhoneWidgets') {
+      setModalHeader('Phone Widgets');
+      setModalPar('A simple app made with HTML, CSS and JavaScript. The app is fully responsive and works on all devices.');
+    }
+  };
+
+  if(modal) {
+    document.body.classList.add('active-modal');
+  } else {
+    document.body.classList.remove('active-modal');
+  }
+  
   return (
-    <div className="Projects" id="projects">
-      <h1>My Projects</h1>
-      <div id="img_1" className="wrapperMain">
+    <>
+      <div className="Projects" id="projects">
+        <h1>My Projects</h1>
         <div className="wrapper">
-          <div className="box" id="SnakeTHEgame"><span>Snake the game</span></div>
-          <div className="box" id="Calendar"><span>Calendar</span></div>
+          <button className="box" id="SnakeTHEgame" onClick={() => toggleModal("SnakeTHEgame")}><span>Snake the game</span></button>
+          <button className="box" id="Calendar" onClick={() => toggleModal("Calendar")}><span>Calendar</span></button>
         </div>
         <div className="wrapper">
-          <div className="box" id="GoodToKnow"><span>GoodToKnow</span></div>
-          <div className="box" id="PhoneWidgets"><span>Phone Widgets</span></div>
+          <button className="box" id="GoodToKnow" onClick={() => toggleModal("GoodToKnow")}><span>GoodToKnow</span></button>
+          <button className="box" id="PhoneWidgets" onClick={() => toggleModal("PhoneWidgets")}><span>Phone Widgets</span></button>
         </div>
       </div>
-    </div>
+      {modal && (
+        <div className="modal">
+          <div className="modal-content">
+            <h2 className='modal-header'>{modalHeader}</h2>
+            <p className='modal-par'>{modalPar}</p>
+            <button className="close-snake" onClick={() => toggleModal()}>CLOSE</button>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 export default Projects
