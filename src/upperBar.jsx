9b1bfe0691af21b88cb99425dfb53436d0c3c15b
lis531 <lisekminecraftowy175@gmail.com>
2023-03-12 11:34:48 +0100

    update #8.5
    dark and light mode are working, even better responsiveness, some small fixes

--- src/upperBar.jsx
+++ src/upperBar.jsx
@@ -1,7 +1,29 @@
 import { VscColorMode } from 'react-icons/vsc';
 import { HiOutlineArrowDown } from 'react-icons/hi';
+import React, { useState } from "react";
+import './index.css';
 
 function upperBar() {
+    const [colorMode, setColorMode] = useState("light");
+    const [state, setState] = useState("light");
+    //change --font-color and --color-background of the page to dark or light respectively
+    if (state === "light") {
+        document.documentElement.style.setProperty('--font-color', '#e0e0e0');
+        document.documentElement.style.setProperty('--color-background', '#1F1F1F');
+        document.documentElement.style.setProperty('--bar-color', '#161616');
+        document.documentElement.style.setProperty('--box-shadow-color', '#161616');
+        document.documentElement.style.setProperty('--text-shadow-color', '#353535');
+        document.documentElement.style.setProperty('--input-color', '#1B1B1B');
+    }
+    else {
+        document.documentElement.style.setProperty('--font-color', '#1F1F1F');
+        document.documentElement.style.setProperty('--color-background', '#e0e0e0');
+        document.documentElement.style.setProperty('--bar-color', '#e9e9e9');
+        document.documentElement.style.setProperty('--box-shadow-color', '#e9e9e9');
+        document.documentElement.style.setProperty('--text-shadow-color', '#cacaca');
+        document.documentElement.style.setProperty('--input-color', '#e4e4e4');
+    }
+
     return(
         <>
             <div className="barStatic">
@@ -12,7 +34,10 @@ function upperBar() {
                         <a href="#contact">Contact</a>
                     </div>
                     <div className='div2'>
-                        <button className="modeChange"><VscColorMode /></button>
+                        <button className="modeChange" onClick={() => {
+                            setColorMode(colorMode === "light" ? "dark" : "light");
+                            setState(colorMode === "light" ? "dark" : "light");
+                        }}><VscColorMode /></button>
                         {/*<div className="dropdown">
                             <button className="dropbtn"><HiOutlineArrowDown/></button>
                             <div className="dropdown-content">
