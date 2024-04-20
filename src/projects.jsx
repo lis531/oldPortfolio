ab3e64ddea5904ab3ac80b07b34ea4971bf07db6
Borys Gajewski <82408934+lis531@users.noreply.github.com>
2024-04-20 20:06:46 +0000

    Update CSS styles and add conditional logic for theme mode

--- src/projects.jsx
+++ src/projects.jsx
@@ -81,23 +81,18 @@ const Projects = () => {
       <h1 ref={el => elements.current[0] = el}><span className="anim">M</span><span className="anim">y</span><span className='space'></span><span className="anim">P</span><span className="anim">r</span><span className="anim">o</span><span className="anim">j</span><span className="anim">e</span><span className="anim">c</span><span className="anim">t</span><span className="anim">s</span></h1>
       <div className="sitePart" id="projects">
         <button onClick={() => toggleModal('Borufi')} className="box">
-          <img src='../borufi.png'></img>
           <h3>Borufi</h3>
         </button>
         <button onClick={() => toggleModal('SchoolProjects')} className="box">
-          <img src='../schoolProjectBg.png'></img>
           <h3>SchoolProjects</h3>
         </button>
         <button onClick={() => toggleModal('TechNews')} className="box">
-          <img src='../techNewsBg.png'></img>
           <h3>TechNews</h3>
         </button>
         <button onClick={() => toggleModal('DiscordBot')} className="box">
-          <img src='../discord.png'></img>
-          <h3>Discord</h3>
+          <h3>Discord Bot</h3>
         </button>
         <button onClick={() => toggleModal('SnakeTHEgame')} className="box">
-          <img src='../snake.png'></img>
           <h3>SnakeTHEgame</h3>
         </button>
       </div>
